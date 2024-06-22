47f673fb928828a2161257a9ba2809050da5ac69
s3afeen <zaidsafeen30@gmail.com>
2024-06-23 00:50:11 +0300

    js-6

---
+++ task6.js
@@ -0,0 +1,68 @@
+//Local Storage****************************************
+//----------------------1
+// localStorage.setItem = ( "string", "Hello, World!");
+// let x = window.localStorage.getItem("string");
+// document.getElementById("www").innerHTML = x;
+
+// localStorage.clear()
+
+
+//----------------------2
+// window.localStorage.setItem("num", 123);
+// let x = window.localStorage.getItem("num");
+// document.getElementById("www").innerHTML = x;
+
+// localStorage.clear()
+
+
+//----------------------3
+// window.localStorage.setItem("bool", true);
+// let x = localStorage.getItem("bool");
+// document.getElementById("www").innerHTML = x;
+
+
+
+//----------------------4
+// localStorage.setItem("naame", "zaid");
+// localStorage.removeItem("naame");
+
+
+
+//----------------------5
+// localStorage.clear()
+
+
+
+
+
+
+//Session Storage****************************************
+//----------------------1
+// window.sessionStorage.setItem("string", "welcom");
+// let x = window.sessionStorage.getItem("string");
+// document.getElementById("www").innerHTML = x;
+
+
+
+//----------------------2
+// window.sessionStorage.setItem("num", 123);
+// let x = window.sessionStorage.getItem("num");
+// document.getElementById("www").innerHTML = x;
+
+
+
+//----------------------3
+// window.sessionStorage.setItem("bool", true);
+// let x = window.sessionStorage.getItem("bool");
+
+
+
+//----------------------4
+// window.sessionStorage.setItem("bool", true);
+// sessionStorage.removeItem("bool");
+
+
+
+//----------------------5
+// window.sessionStorage.setItem("bool", true);
+// sessionStorage.clear();
